682dcfd39c77c7a1c608e627740279263b277336
jonathanrlouie <jonathanrlouie@gmail.com>
2022-05-23 16:23:22 -0700

    Make functional tests step fail in CI if a test fails

--- run_functional_tests.sh
+++ run_functional_tests.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 FUNCTIONAL_TEST_PATH="src/test/functional"
 
+errs=0
 run_test() {
-  cd $FUNCTIONAL_TEST_PATH/$1 && mvn idris:compile idris:run
+  cd $FUNCTIONAL_TEST_PATH/$1 && mvn idris:compile idris:run || ((errs++))
   cd -
 }
 
+# Install the maven plugin first before running tests
 mvn install
-run_test dependencies-test
+
+# If there is a test failure, run every functional test before exiting
+run_test dependencies-test 
+
+exit $errs
