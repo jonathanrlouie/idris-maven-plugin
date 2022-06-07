cce89ac66d2b2306524c622ce7916828cebcee60
jonathanrlouie <jonathanrlouie@gmail.com>
2022-06-06 23:32:58 -0700

    Add VSCode and plugins to dev shell

--- shell.nix
+++ shell.nix
@@ -1,6 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-  buildInputs = with pkgs; [ jdk8 wget unzip maven ];
+  buildInputs = with pkgs; [
+    jdk11
+    maven
+    (vscode-with-extensions.override {
+      vscodeExtensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        eamodio.gitlens
+        redhat.java
+      ];
+    })
+  ];
   idrisJvm = pkgs.fetchzip {
     url = "https://github.com/mmhelloworld/idris-jvm/releases/download/v0.5.1/idris2-0.5.1-SNAPSHOT.zip";
     sha256 = "105ni64cygc73nsy07rbk1c1blj60cx3ci6glya53zjp83hvxhmm";
