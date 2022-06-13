1efdc1d8baf4baf0845a3efdfef4994ddbe57db1
jonathanrlouie <jonathanrlouie@gmail.com>
2022-06-12 22:56:30 -0700

    Update URL of shell.nix since old idris-jvm release is no longer available at original URL

--- shell.nix
+++ shell.nix
@@ -12,7 +12,7 @@ pkgs.mkShell {
     })
   ];
   idrisJvm = pkgs.fetchzip {
-    url = "https://github.com/mmhelloworld/idris-jvm/releases/download/v0.5.1/idris2-0.5.1-SNAPSHOT.zip";
+    url = "https://github.com/mmhelloworld/idris-jvm/releases/download/v0.5.1-rc.2/idris2-0.5.1-SNAPSHOT.zip";
     sha256 = "105ni64cygc73nsy07rbk1c1blj60cx3ci6glya53zjp83hvxhmm";
   };
   shellHook = ''
