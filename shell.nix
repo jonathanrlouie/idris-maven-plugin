4f9a1234b5def295dc9f7f5199b0cebfb8bdab08
jonathanrlouie <jonathanrlouie@gmail.com>
2022-07-15 09:56:09 -0700

    Revert to using nix develop for CI

--- shell.nix
+++ shell.nix
@@ -3,13 +3,6 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     jdk11
     maven
-    (vscode-with-extensions.override {
-      vscodeExtensions = with pkgs.vscode-extensions; [
-        vscodevim.vim
-        eamodio.gitlens
-        redhat.java
-      ];
-    })
   ];
   idrisJvm = pkgs.fetchzip {
     url = "https://github.com/mmhelloworld/idris-jvm/releases/download/v0.5.1-rc.2/idris2-0.5.1-SNAPSHOT.zip";
