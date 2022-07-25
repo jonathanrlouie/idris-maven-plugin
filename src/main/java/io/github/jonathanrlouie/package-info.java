50cc1d3277cc4c62e8a69efd67747736053c3a01
jonathanrlouie <jonathanrlouie@gmail.com>
2022-07-24 17:22:53 -0700

    Update package-info.java

--- src/main/java/io/github/jonathanrlouie/package-info.java
+++ src/main/java/io/github/jonathanrlouie/package-info.java
@@ -1,1 +1,9 @@
+/**
+ * This module contains all of the Mojos for compiling and running Idris 2 projects.
+ *
+ * @since SNAPSHOT-1.0
+ * @author jonathanrlouie
+ * @version SNAPSHOT-1.0
+ */
 package io.github.jonathanrlouie;
+
