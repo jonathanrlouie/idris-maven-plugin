dde1bcba7539e6feecb821239079bdbc341bc8bb
jonathanrlouie <jonathanrlouie@gmail.com>
2024-01-01 00:56:36 -0800

    Fix tests in CI pipeline

--- shell.nix
+++ shell.nix
@@ -4,7 +4,11 @@ pkgs.mkShell {
     jdk11
     maven
   ];
+  idrisJvm = pkgs.fetchzip {
+    url = "https://github.com/mmhelloworld/idris-jvm/releases/download/v0.5.1-rc.2/idris2-0.5.1-SNAPSHOT.zip";
+    sha256 = "105ni64cygc73nsy07rbk1c1blj60cx3ci6glya53zjp83hvxhmm";
+  };
   shellHook = ''
-    export IDRIS2_PREFIX=lib
+    export IDRIS2_PREFIX=$idrisJvm/lib
   '';
 }
